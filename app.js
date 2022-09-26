446160b2d9f1dcc3a61c99f5381f0ede5213c243
Charmaine Kwok <69842891+charmaine-kwok@users.noreply.github.com>
2022-09-26 17:31:16 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container, .movie-list-title, .logo, .navbar-container, .sidebar, .left-menu-icon, .toggle");
+
+ball.addEventListener("click", () => {
+    items.forEach(item => {
+        item.classList.toggle("active");
+    })
+    ball.classList.toggle("active");
+})
