ad7e160f718475cab6ed77f8f2d0a93f7381de02
Charmaine Kwok <69842891+charmaine-kwok@users.noreply.github.com>
2022-09-27 10:54:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(".container, .movie-list-title, .logo, .navbar-container, .sidebar, .left-menu-icon, .toggle");
+const items = document.querySelectorAll(".container, .movie-item, .movie-list-title, .logo, .navbar-container, .sidebar, .left-menu-icon, .toggle");
 
 ball.addEventListener("click", () => {
     items.forEach(item => {
